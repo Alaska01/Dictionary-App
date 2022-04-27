81e2faa29956d01217f5215ef661c4c6262ac5e6
Alaska01 <aadaniel108@gmail.com>
2022-04-27 12:11:49 +0100

    Install axios to fetch data for random word

--- src/components/Newword.js
+++ src/components/Newword.js
@@ -1,14 +1,51 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 function Newword() {
 
-  const [word, setWord] = useState('')
-  fetch(`https://random-words-api.vercel.app/word`)
-  .then(response=>response.json())
-  .then(data=>console.log(data))
+  
+  const [loading, setLoading] = useState(false);
+  const [word, setWord] = useState([]);
+
+  // fetch(`https://random-words-api.vercel.app/word`)
+  // .then(response=>response.json())
+  // .then(data=>console.log(data))
+
+  useEffect(() => {
+    const loadRandomWord = async () => {
+
+        // Till the data is fetch using API 
+        // the Loading page will show.
+        setLoading(true);
+
+        // Await make wait until that 
+        // promise settles and return its result
+        const response = await axios.get(
+        "https://random-words-api.vercel.app/word");
+
+        // After fetching data stored it in posts state.
+        setWord(response.data);
+
+        // Closed the loading page
+        setLoading(false);
+    }
+
+    // Call the function
+    loadRandomWord();
+}, []);
+
   return ( 
     <>
-      <h1> New Display</h1>
+      {/* {loading ? (
+                    <h4>Loading...</h4>) :
+                    (posts.map((item) =>
+                        // Presently we only fetch 
+                        // title from the API 
+                        <h4>{item.title}</h4>)
+                    )
+                } */}
+
+                {loading? (<h4>Loading new word...</h4>) : (<h4>word</h4>)}
     </>
    );
 }
